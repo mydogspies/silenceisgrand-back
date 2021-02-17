856ac0f0937e87b5fcdb6a9cc2024984de39950b
mydogspies <mydogspies@zoho.com>
2021-02-17 11:32:09 +0100

    Added asyncHandler to get clean up a bunch of try/catch blocks in the controllers

--- controller/blogs.js
+++ controller/blogs.js
@@ -1,30 +1,23 @@
 const Blog = require('../model/Blog');
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 
 // @desc    Get all blogs
 // @route   GET /api/v1/blogs
 // @access  Public
-exports.getAllBlogs = async (req, res, next) => {
-    try {
+exports.getAllBlogs = asyncHandler(async(req, res, next) => {
         const blogs = await Blog.find();
-        if (blogs.length === 0) {
-            return next(new ErrorResponse(`No blogs found!`, 404));
-        }
         res.status(200).json({
             success: true,
             count: blogs.length,
             data: blogs
         });
-    } catch (err) {
-        next(err);
-    }
-}
+});
 
 // @desc    Get a blog by its _id
 // @route   GET /api/v1/blogs/:id
 // @access  Public
-exports.getBlogById = async (req, res, next) => {
-    try {
+exports.getBlogById = asyncHandler(async (req, res, next) => {
         const blog = await Blog.findById(req.params.id);
         if (!blog) {
             return next(new ErrorResponse(`No blogs found!`, 404));
@@ -33,31 +26,23 @@ exports.getBlogById = async (req, res, next) => {
             success: true,
             data: blog
         })
-    } catch (err) {
-       next(err);
-    }
-}
+});
 
 // @desc    Create a new entry of type Blog
 // @route   POST /api/v1/blogs
 // @access  Private
-exports.createBlog = async (req, res, next) => {
-    try {
+exports.createBlog = asyncHandler(async (req, res, next) => {
         const blog = await Blog.create(req.body);
         res.status(200).json({
             success: true,
             data: blog
         })
-    } catch (err) {
-        next(err);
-    }
-}
+});
 
 // @desc    Update an existing Blog using its id
 // @route   PUT /api/v1/blogs/:id
 // @access  Private
-exports.updateBlog = async (req, res, next) => {
-    try {
+exports.updateBlog = asyncHandler(async (req, res, next) => {
         const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
@@ -69,16 +54,12 @@ exports.updateBlog = async (req, res, next) => {
             success: true,
             data: blog
         })
-    } catch (err) {
-        next(err);
-    }
-}
+});
 
 // @desc    Delete a blog using its id
 // @route   DELETE /api/v1/blogs/:id
 // @access  Private
-exports.deleteBlog = async (req, res, next) => {
-    try {
+exports.deleteBlog = asyncHandler(async (req, res, next) => {
         const blog = await Blog.findByIdAndDelete(req.params.id);
         if (!blog) {
             return next(new ErrorResponse(`Blog not found with id ${req.params.id}`, 404));
@@ -87,8 +68,4 @@ exports.deleteBlog = async (req, res, next) => {
             success: true,
             data: {}
         })
-    } catch (err) {
-        next(err);
-    }
-
-}
+});
