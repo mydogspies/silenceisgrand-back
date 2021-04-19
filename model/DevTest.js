9ba9f09479c9b938466719c53f5022835bb4c411
mydogspies <mydogspies@zoho.com>
2021-04-19 22:24:49 +0200

    added the base logic for mysql using sequelize

--- model/DevTest.js
+++ model/DevTest.js
@@ -1,7 +1,8 @@
 module.exports = (sequelize, Sequelize) => {
     const DevTest = sequelize.define("devtest", {
         id: {
-            type: Sequelize.INTEGER
+            type: Sequelize.INTEGER,
+            primaryKey: true
         },
         test: {
             type: Sequelize.STRING
